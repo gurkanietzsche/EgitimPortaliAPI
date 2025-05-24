41a2c12f62ea3d85390ec6bbb903202d7659637a
Gürkan Yavuz <148501770+gurkanietzsche@users.noreply.github.com>
2025-05-24 19:30:50 +0300

    Delete EgitimPortali.UI directory

--- EgitimPortali.UI/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EgitimPortali.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
